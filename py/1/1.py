1ae37129b302f0aabb7d61fcd31b849c11541f85
Leonid Mashinskiy <LeoMash@gmail.com>
2024-12-01 08:10:17 +0300

    Day 1 solution

--- py/1/1.py
+++ py/1/1.py
@@ -1,6 +1,27 @@
-import sys
-# f = sys.argv[1]
+from collections import defaultdict
+
 f = '1.in'
 D = open(f).read().strip()
+d1 = []
+d2 = []
+d2freq = defaultdict(int)
 for line in D.split('\n'):
+    a, b = line.split()
+    a = int(a)
+    b = int(b)
+    d1.append(a)
+    d2.append(b)
+    d2freq[b] += 1
     pass
+
+d1 = sorted(d1)
+d2 = sorted(d2)
+
+sum1 = 0
+sum2 = 0
+for a, b in zip(d1, d2):
+    sum1 += abs(a - b)
+    sum2 += a * d2freq[a]
+
+print(sum1)
+print(sum2)
