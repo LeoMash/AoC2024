8863a9a6835d34a9c81d34c998a71c2671485e31
Leonid Mashinskiy <LeoMash@gmail.com>
2024-12-07 13:20:30 +0300

    Day 7 backward recursion

--- py/7/7.py
+++ py/7/7.py
@@ -33,6 +33,32 @@ def test2(res, xs):
     return go(xs[0], 1)
 
 
+def test2_opt(res, xs):
+    start = xs[0]
+    def go(cur, idx):
+        if cur < start:
+            return False
+        if idx == 0:
+            return cur == start
+
+        x = xs[idx]
+        if cur % x == 0:
+            if go(cur // x, idx - 1):
+                return True
+
+        sx = str(x)
+        scur = str(cur)
+        if scur.endswith(sx):
+            ncur = scur[:-len(sx)]
+            if ncur and go(int(ncur), idx - 1):
+                return True
+
+        return go(cur - x, idx - 1)
+
+    return go(res, len(xs) - 1)
+
+
+
 D = open(f).read().split('\n')
 F = []
 for line in D:
@@ -42,15 +68,26 @@ for line in D:
     # print(a, nums)
     F.append((a, nums))
 
-tm = time.time()
 ans = 0
-ans2 = 0
 for a, nums in F:
     if test(a, nums):
         ans += a
+print(ans)
+
+tm = time.time()
+ans2 = 0
+for a, nums in F:
     if test2(a, nums):
         ans2 += a
 
 print(time.time() - tm)
-print(ans)
 print(ans2)
+
+tm = time.time()
+ans3 = 0
+for a, nums in F:
+    if test2_opt(a, nums):
+        ans3 += a
+
+print(time.time() - tm)
+print(ans3)
