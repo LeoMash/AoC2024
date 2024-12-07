831a7c2fe10e1da269df04283fec7da1677e3a2f
Leonid Mashinskiy <LeoMash@gmail.com>
2024-12-07 12:51:02 +0300

    Day 7 recursion optimization

--- py/7/7.py
+++ py/7/7.py
@@ -1,49 +1,56 @@
+import time
+
 f = '7.in'
 # f = '7_test.in'
 
 
 def test(res, xs):
     n = len(xs)
-    def dfs(cur, idx):
+
+    def go(cur, idx):
+        if cur > res:
+            return False
         if idx == n:
             return cur == res
-        if dfs(cur + xs[idx], idx + 1):
-            return True
-        if dfs(cur * xs[idx], idx + 1):
-            return True
-        return False
+        return go(cur + xs[idx], idx + 1) or \
+            go(cur * xs[idx], idx + 1)
 
-    return dfs(0, 0)
+    return go(xs[0], 1)
+
 
 def test2(res, xs):
     n = len(xs)
-    def dfs(cur, idx):
+
+    def go(cur, idx):
+        if cur > res:
+            return False
         if idx == n:
             return cur == res
-        if dfs(cur + xs[idx], idx + 1):
-            return True
-        if dfs(cur * xs[idx], idx + 1):
-            return True
-        if dfs(int(str(cur) + str(xs[idx])), idx + 1):
-            return True
-        return False
+        return go(cur + xs[idx], idx + 1) or \
+            go(cur * xs[idx], idx + 1) or \
+            go(int(str(cur) + str(xs[idx])), idx + 1)
 
-    return dfs(0, 0)
+    return go(xs[0], 1)
 
 
 D = open(f).read().split('\n')
 F = []
-ans = 0
-ans2 = 0
 for line in D:
     a, lst = line.split(':')
     a = int(a)
     nums = list(map(int, lst.split()))
-    print(a, nums)
+    # print(a, nums)
+    F.append((a, nums))
+
+tm = time.time()
+ans = 0
+ans2 = 0
+for a, nums in F:
     if test(a, nums):
         ans += a
     if test2(a, nums):
         ans2 += a
 
+print(time.time() - tm)
 print(ans)
 print(ans2)
