41f7934207d8a9b88916b1ce0bee33bebece8c9d
Leonid Mashinskiy <LeoMash@gmail.com>
2024-12-13 08:21:16 +0300

    Day 13 solution

---
+++ py/13/13.py
@@ -0,0 +1,52 @@
+from collections import defaultdict
+
+f = '13.in'
+# f = '13_test.in'
+
+D = open(f).read().split('\n')
+D.append('')
+
+
+def solve1(a, b, c):
+    a1, a2 = a
+    b1, b2 = b
+    c1, c2 = c
+
+    D1 = (c1 * b2 - c2 * b1)
+    D2 = (c1 * a2 - c2 * a1)
+    D = (a1*b2 - a2*b1)
+    if D1 % D == 0 and D2 % D == 0:
+        x = D1 /D
+        y = -D2 / D
+        return x, y
+    return None, None
+
+SHIFT = 10000000000000
+
+ans1 = 0
+ans2 = 0
+for idx in range(0, len(D), 4):
+    a = D[idx].split(':')[1].split(',')
+    a = [int(x.split('+')[1]) for x in a]
+    b = D[idx + 1].split(':')[1].split(',')
+    b = [int(x.split('+')[1]) for x in b]
+    c = D[idx + 2].split(':')[1].split(',')
+    c = [int(x.split('=')[1]) for x in c]
+
+    print(a, b, c)
+
+    x, y = solve1(a, b, c)
+    print(x, y)
+    if x is not None:
+        print(x * 3 + y)
+        ans1 += x * 3 + y
+    c[0] += SHIFT
+    c[1] += SHIFT
+    x, y = solve1(a, b, c)
+    print(x, y)
+    if x is not None:
+        print(x * 3 + y)
+        ans2 += x * 3 + y
+
+print(ans1)
+print(ans2)
